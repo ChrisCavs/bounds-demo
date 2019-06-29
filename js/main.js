a0c47fb47cc56792caa3cf3aaeff39fd30d394d3
Christopher Cavalea <chris211321@gmail.com>
2019-06-28 20:47:33 -0400

    updated demo

--- js/main.js
+++ js/main.js
@@ -1,36 +1,47 @@
-import Bounds from '../../bounds.js/src/bounds.js'
+import 'intersection-observer'
+import Bounds from 'bounds.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const box = document.querySelector('.box')
-  const circles = document.querySelectorAll('.circle')
-  const onCircleEnter = (circle) => {
-    return () => {
-      console.log('circle entered')
-      circle.classList.add('red')
-    }
-  }
-  const onCircleLeave = (circle) => {
-    return () => {
-      console.log('circle left')
-      circle.classList.remove('red')
-    }
-  }
+  const circle = document.querySelector('.circle')
+  const textBoxes = document.querySelectorAll('.text-box')
+  const images = document.querySelectorAll('img')
 
   const windowBound = Bounds({
-    margins: { bottom: 150},
+    margins: { bottom: 100},
   })
   const boxBound = Bounds({
     root: box,
     threshold: 1.0,
   })
-  console.log(boxBound)
 
-  circles.forEach(circle => {
-    boxBound.watch(
-      circle, onCircleEnter(circle), onCircleLeave(circle)
-    )
-    circle.addEventListener('transitionend', () => {
-      circle.classList.toggle('move')
-    })
+  const onImageEnter = (image) => {
+    return () => {
+      image.src = image.dataset.src
+      textBoxes[parseInt(image.dataset.index)].classList.add('show')
+      windowBound.unWatch(image)
+    }
+  }
+
+  const onCircleEnter = () => {
+    circle.style['background-color'] = '#ffae00'
+  }
+  const onCircleLeave = () => {
+    circle.style['background-color'] = 'lightgray'
+  }
+
+  boxBound.watch(
+    circle, onCircleEnter, onCircleLeave
+  )
+  images.forEach(image => {
+    windowBound.watch(image, onImageEnter(image))
+  })
+
+  circle.addEventListener('transitionend', () => {
+    circle.classList.toggle('move')
+  })
+
+  setTimeout(() => {
+    circle.classList.toggle('move')
   })
 });
